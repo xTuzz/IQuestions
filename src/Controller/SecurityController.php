2217c72aca09f32f51c616c1905b469913cdda36
Allanee <allane61260@gmail.com>
2022-10-28 10:47:29 +0200

    enlever la redirection

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,15 +7,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        if ($this->getUser()) {
-             return $this->redirectToRoute('app_user_index/{id}');
-         }
-
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
