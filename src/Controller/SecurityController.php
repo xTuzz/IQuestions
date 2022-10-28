81fdbfc7d3482c35791bcc0a0295c92320c5e71c
Allanee <allane61260@gmail.com>
2022-10-28 10:48:04 +0200

    remettre en comm la redirection

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,6 +13,10 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        /*if ($this->getUser()) {
+             return $this->redirectToRoute('homepage');
+         }*/
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
